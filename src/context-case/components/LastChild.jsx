c9d67a2e6d0a78d777d29f3dd454dcfe2e94ee70
rakibshakib <rakibshakib74@gmail.com>
2025-01-22 01:17:50 +0600

    update: react query state management

--- src/context-case/components/LastChild.jsx
+++ src/context-case/components/LastChild.jsx
@@ -7,7 +7,10 @@ const LastChild = () => {
     <div className="flex items-center justify-center mx-auto">
       <div className="w-[250px] py-5 rounded-md px-5 border-white shadow-lg bg-purple-100 text-black flex flex-col items-center gap-2">
         <p className="text-lg font-bold">{user.name || "N/A"}</p>
-        <p>Status: {user.isActive ? "Active" : "Inactive"}</p>
+        <p>
+          Status: {user.isActive ? "Active" : "Inactive"}
+          <span>{user?.isActive ? " 🟢" : " 🔴"}</span>{" "}
+        </p>
       </div>
     </div>
   );
--- src/ts-query/components/User.jsx
+++ src/ts-query/components/User.jsx
@@ -6,7 +6,10 @@ const User = () => {
     <div className="flex items-center justify-center mx-auto">
       <div className="w-[250px] py-5 rounded-md px-5 border-white shadow-lg bg-purple-100 text-black flex flex-col items-center gap-2">
         <p className="text-lg font-bold">{data?.name || "N/A"}</p>
-        <p>Status: {data?.isActive ? "Active" : "Inactive"}</p>
+        <p>
+          Status: {data?.isActive ? "Active" : "Inactive"}{" "}
+          <span>{data?.isActive ? " 🟢" : " 🔴"}</span>{" "}
+        </p>
       </div>
     </div>
   );
